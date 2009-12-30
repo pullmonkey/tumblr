798dd913631612a6f8ad392acd68f3394d95916d
Pullmonkey <charlie@pullmonkey.com>
2009-12-30 19:50:41 +0000

    need the options to be part of the query k/v pair and add support for authenticated reads to get private posts

--- lib/tumblr/request.rb
+++ lib/tumblr/request.rb
@@ -3,7 +3,15 @@ class Tumblr
     
     # a GET request to http://[YOURUSERNAME].tumblr.com/api/read
     def self.read(options = {})
-        response = HTTParty.get("http://#{Tumblr::blog ||= 'staff'}.tumblr.com/api/read", options)
+      # enable authenticated reads for private posts
+      if options.has_key?(:email) and options.has_key?(:password) 
+        # if we have both email and password, then we need to do a POST thus authenticating
+        # TODO but this does not work because of the Post::count method - if all posts are private 
+        # then the total returned in count will always be 0 which means we will never get here.
+        response = HTTParty.post("http://#{Tumblr::blog ||= 'staff'}.tumblr.com/api/read", :query => options)
+      else
+        response = HTTParty.get("http://#{Tumblr::blog ||= 'staff'}.tumblr.com/api/read", :query => options)
+      end
       return response unless raise_errors(response)
     end
     
